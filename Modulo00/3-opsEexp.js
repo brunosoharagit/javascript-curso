36c1aa42c4fafe6a679873bd7f2cde905e7a0b2e
brunosoharagit <b.sohara.git@gmail.com>
2022-09-28 10:50:08 -0300

    OBC: MOdulo00 >> Operações, epressões e comentários

---
+++ Modulo00/3-opsEexp.js
@@ -0,0 +1,40 @@
+//ops. aritméticas
+console.log(1+2);
+console.log(1-2);
+console.log(1*2);
+console.log(1/2);
+console.log(1%2);
+
+console.log();
+
+//incremento
+let a = 3;
+console.log(a+=3); //retorna 6
+console.log(a-=2); //retorna 4
+console.log(a*=2); //retorna 8
+console.log(a/=2); //retorna 4
+console.log(a%=2); //retorna 0
+
+a++;
+console.log(a);
+a++;
+console.log(a);
+
+console.log();
+
+//ops. lógicos
+console.log(1<2);
+console.log(1>2);
+console.log(2!=1);
+
+console.log()
+
+//op. de concatenação
+let nome = "Bruno";
+let sobrenome = "Sohara";
+
+console.log(nome + " " + sobrenome);
+
+console.log(1 + "1");
+console.log(parseInt(1 + "1"));
+
