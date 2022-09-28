374d3e57400dd49674d3f21af31a0e52ca92e651
brunosoharagit <b.sohara.git@gmail.com>
2022-09-28 10:30:24 -0300

    OBC: modulo00 >> alterações

---
+++ Modulo00/2-variaveis.js
@@ -0,0 +1,19 @@
+//var --> forma antiga de variável em JS
+//let --> forma moderna de variável em JS
+let dia;
+console.log(dia);
+
+dia = 28;
+console.log(dia);
+
+//const --> forma para que mantenha o valor da variável
+const mes = "Junho";
+console.log(mes);
+
+mes = "Setembro";
+console.log(mes);
+
+
+//Nomeação de variáveis
+let nomeCliente = "João Fernandes";
+console.log(nomeCliente);
